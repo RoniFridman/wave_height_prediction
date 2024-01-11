fabac35126d0ab1953768c8e23ccf634483f903d
RoniFridman1 <ronifridman0@gmail.com>
2024-01-11 16:30:46 +0200

    Debugging 2

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def load_data(data, features_mask='all', forecast_lead=4, target_variable='hs', 
     data['direction_y'] = data['direction'].apply(lambda x: np.sin(x / 360))
     data['wind_direction_x'] = data['wind_direction'].apply(lambda x: np.cos(x / 360))
     data['wind_direction_y'] = data['wind_direction'].apply(lambda x: np.sin(x / 360))
-    data.drop(columns=['direction'], inplace=True)
+    data.drop(columns=['direction', "wind_direction"], inplace=True)
 
     # Do we want all features or filtering some of them. Default is "all".
     if features_mask != 'all':
@@ -73,6 +73,9 @@ def create_short_data_csv(full_csv_path,wind_data_path,swell_data_path, new_data
     wind_data.index = pd.to_datetime(wind_data.index,utc=True)
     wind_data = wind_data.resample('30min').mean()
     swell_data = pd.read_csv(swell_data_path, index_col='datetime')
+    swell_data.index = pd.to_datetime(swell_data.index,utc=True)
+    swell_data = swell_data.resample('30min').mean()
+
 
     if predict_latest:
         data = pd.read_csv(full_csv_path).set_index('datetime').iloc[-1 * forecast * seq_number:]
@@ -82,7 +85,7 @@ def create_short_data_csv(full_csv_path,wind_data_path,swell_data_path, new_data
     cutoff_index= round(-17520 * years_of_data_to_use)
     data = pd.read_csv(full_csv_path,index_col='datetime').iloc[cutoff_index:]
     data.index = pd.to_datetime(data.index)
-    data = pd.DataFrame.join(data, wind_data, on='datetime', how='left')
+    data = pd.DataFrame.join(data, wind_data, on='datetime', how='left').join(other=swell_data,on='datetime', how='left')
     data[-1 * forecast * seq_number:-1 * forecast].to_csv(new_data_training_path)
     data[-1 * forecast:].to_csv(empirical_test_data_path)
     data = data.iloc[:-1 * forecast]
@@ -153,12 +156,12 @@ def update_latest_data_from_db(full_data_path, location='haifa'):
             file.close()
 
         swell_data_table_name = "waves_data.swell"
-        command = f"COPY (select datetime, hs_swell from {wind_data_table_name} where location = {location} order by datetime) " \
+        command = f"COPY (select datetime, hs_swell from {swell_data_table_name} where location = '{location}' order by datetime) " \
                   f"to STDOUT with CSV delimiter ',' header"
         command = cursor.mogrify(command)
         connection.commit()
-        wind_csv_output_path = full_data_path[:-4] + "_swell.csv"
-        with open(wind_csv_output_path, "w+") as file:
+        swell_csv_output_path = full_data_path[:-4] + "_swell.csv"
+        with open(swell_csv_output_path, "w+") as file:
             cursor.copy_expert(command, file)
             file.close()
 
